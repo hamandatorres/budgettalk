1ddd4a3ef3af57be6a81a0a04f3e04530000c836
Carlos A Chavez Perez <hamandatorres@gmail.com>
2025-08-18 17:24:34 -0600

    Refactor Arena component to use environment variable for API URL and improve axios requests

--- src/components/Arena.js
+++ src/components/Arena.js
@@ -1,30 +1,36 @@
-import React, {Component} from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
+
+const API_URL =
+	process.env.REACT_APP_API_URL ||
+	"https://evening-cliffs-17109-56706eeb61a8.herokuapp.com";
 
 class Arena extends Component {
+	// this.setState = ({ this.arenaCp: this.props.moveCP})
+	componentDidMount() {
+		this.getCp();
+		console.log(this.state.cp);
+	}
+	getCp = () => {
+		axios
+			.get(`${API_URL}/api/councilperson`)
+			.then((response) => {
+				this.setState({ arenaCp: response.data });
+			})
+			.catch((error) => console.log(error));
+	};
 
-  // this.setState = ({ this.arenaCp: this.props.moveCP})
-  componentDidMount() {
-        this.getCp();
-        console.log(this.state.cp)
-      }
-      getCp = () => {
-        axios.get('/api/councilperson')
-      .then(response => {
-          this.setState({ arenaCp: response.data })
-      })
-      .catch(error => console.log(error))
-    }
-      moveCp(cp){
-          axios.post(`http://localhost:3001/api/councilperson`, {cp: cp})
-        .then(response => {
-          this.setState({ arenaCp: response.data})
-        })
-        .catch(error => console.log(error))
-      }
-  render() {
-    return
-  }
+	moveCp(cp) {
+		axios
+			.post(`${API_URL}/api/councilperson`, { cp: cp })
+			.then((response) => {
+				this.setState({ arenaCp: response.data });
+			})
+			.catch((error) => console.log(error));
+	}
+	render() {
+		return;
+	}
 }
 
 export default Arena;
